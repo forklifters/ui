62de53e31546a6427e6424767b9bb745cf8f39cb
Alison Chan <alison.chan@thinkful.com>
2019-11-14 18:09:53 -0500

    concierge image + a bit of style faffing

--- src/AppBar/ConciergeModal.jsx
+++ src/AppBar/ConciergeModal.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ConciergeImage from './ConciergeImage';
+
 const BetaBadge = () => (
   <span className="tui-concierge-beta-badge">
     Beta
@@ -34,32 +36,41 @@ ConciergeOption.defaultProps = {
 
 const ConciergeModal = () => (
   <div className="tui-concierge-modal">
-    <h2>Thinkful Concierge <BetaBadge /></h2>
-    <p>
-      Immediate resources when you need them most.
-    </p>
+    <div className="tui-concierge-header">
+      <div className="tui-concierge-header-image-container">
+        <ConciergeImage />
+      </div>
+      <div>
+        <h2>Thinkful Concierge <BetaBadge /></h2>
+        <p>
+          Immediate resources when you need them most.
+        </p>
+      </div>
+    </div>
+
     <h3>Stuck? Let's get you some help.</h3>
+    <div className="tui-concierge-options-container">
+      <ConciergeOption
+        title="Start a convo on Slack"
+        subtitle="Pair with other students on the same topics"
+        background="#1733ff"
+        link="https://thinkful.com"
+      />
 
-    <ConciergeOption
-      title="Start a convo on Slack"
-      subtitle="Pair with other students on the same topics"
-      background="#1733ff"
-      link="https://thinkful.com"
-    />
+      <ConciergeOption
+        title="Chat with an expert"
+        subtitle="Talk with a live tutor now"
+        background="#03533d"
+        link="https://thinkful.com"
+      />
 
-    <ConciergeOption
-      title="Chat with an expert"
-      subtitle="Talk with a live tutor now"
-      background="#03533d"
-      link="https://thinkful.com"
-    />
-
-    <ConciergeOption
-      title="Find a Q&amp;A Session"
-      subtitle="Find a session related to what you're working on"
-      background="#ff6b31"
-      link="https://thinkful.com"
-    />
+      <ConciergeOption
+        title="Find a Q&amp;A Session"
+        subtitle="Find a session related to what you're working on"
+        background="#ff6b31"
+        link="https://thinkful.com"
+      />
+    </div>
   </div>
 );
 
