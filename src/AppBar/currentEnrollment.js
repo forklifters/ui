02c30ad84c24ec91494781fce25767062ab5e857
Carlos Edo <carlosedo14@gmail.com>
2020-01-15 19:35:15 +0100

    dont make a request if the contactId is undefined

--- src/AppBar/currentEnrollment.js
+++ src/AppBar/currentEnrollment.js
@@ -1,6 +1,10 @@
 import superagent from 'superagent';
 
 const getCurrentEnrollment = (config, user, successCallback, failCallback) => {
+  if (user.contactId === undefined) {
+    return;
+  }
+
   const url = `${config.api.url}/api/contacts/${user.contactId}/enrollments/current`;
   return superagent
     .get(url)
